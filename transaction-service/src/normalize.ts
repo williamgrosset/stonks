ac912f4dae001a63255ab616fe61e43c05a56ef2
William Grosset <williamhgrosset@gmail.com>
2025-02-15 15:40:35 -0800

    Use Prisma generated models for type-checking in normalization

--- transaction-service/src/normalize.ts
+++ transaction-service/src/normalize.ts
@@ -1,22 +1,48 @@
-export function normalizeStockTransaction(tx: any) {
-  const { id, user_id, price, ...rest } = tx
+import { StockTransaction, WalletTransaction } from 'prisma-client'
+
+type WalletTxWithStockTx = WalletTransaction<{ stock_transaction: { select: { id: true } } }>
+
+interface NormalizedStockTransaction {
+  order_status: string
+  order_type: string
+  quantity: number
+  time_stamp: Date
+  stock_tx_id: string
+  parent_tx_id: string | null
+  wallet_tx_id: string | null
+  stock_id: string
+  stock_price: number
+  is_buy: boolean
+}
+
+interface NormalizedWalletTransaction {
+  is_debit: boolean
+  amount: number
+  time_stamp: Date
+  wallet_tx_id: string
+  stock_tx_id: string | null
+}
+
+export function normalizeStockTransaction(tx: StockTransaction): NormalizedStockTransaction {
+  const { id, user_id, price, stock_id, ...rest } = tx
 
   return {
     ...rest,
-    stock_tx_id: tx.id,
-    parent_tx_id: tx.parent_stock_transaction_id ?? null,
-    wallet_tx_id: tx.wallet_transaction_id ?? null,
+    stock_tx_id: tx.id.toString(),
+    parent_tx_id: tx.parent_stock_transaction_id ? tx.parent_stock_transaction_id.toString() : null,
+    wallet_tx_id: tx.wallet_transaction_id ? tx.wallet_transaction_id.toString() : null,
+    stock_id: tx.stock_id.toString(),
     stock_price: tx.price,
     is_buy: tx.order_type === 'LIMIT' ? true : false
   }
 }
 
-export function normalizeWalletTransaction(tx: any) {
-  const { id, user_id, ...rest } = tx
+export function normalizeWalletTransaction(tx: WalletTxWithStockTx): NormalizedWalletTransaction {
+  const { id, user_id, stock_transaction, ...rest } = tx
 
   return {
     ...rest,
-    wallet_tx_id: tx.id,
-    stock_tx_id: tx.stock_transaction ? tx.stock_transaction.id : null
+    wallet_tx_id: tx.id.toString(),
+    stock_tx_id: tx.stock_transaction ? tx.stock_transaction.id.toString() : null
   }
 }
--- user-service/src/normalize.ts
+++ user-service/src/normalize.ts
@@ -1,12 +1,16 @@
+import { Share } from 'prisma-client'
+
+type ShareWithStock = Share<{ stock: { select: { stock_name: true } } }>
+
 interface NormalizedShare {
   stock_id: string
   stock_name: string
   quantity_owned: number
 }
 
-export function normalizeShare(share: any): NormalizedShare {
+export function normalizeShare(share: ShareWithStock): NormalizedShare {
   return {
-    stock_id: share.stock_id,
+    stock_id: share.stock_id.toString(),
     stock_name: share.stock.stock_name,
     quantity_owned: share.quantity
   }
