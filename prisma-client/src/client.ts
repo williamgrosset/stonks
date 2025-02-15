1bc22492012ccfb2398aa805c4e83e6626004798
William Grosset <williamhgrosset@gmail.com>
2025-02-15 15:21:51 -0800

    Export models from Prisma client

--- prisma-client/src/client.ts
+++ prisma-client/src/client.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client'
+import { Prisma, PrismaClient } from '@prisma/client'
 
 const globalForPrisma = global as unknown as { prisma?: PrismaClient }
 
@@ -15,3 +15,10 @@ if (process.env.NODE_ENV !== 'production') {
   globalForPrisma.prisma = prisma
 }
 
+export type User<T extends Prisma.usersInclude = {}> = Prisma.usersGetPayload<{ include: T }>
+export type Stock<T extends Prisma.stocksInclude = {}> = Prisma.stocksGetPayload<{ include: T }>
+export type Share<T extends Prisma.sharesInclude = {}> = Prisma.sharesGetPayload<{ include: T }>
+export type StockTransaction<T extends Prisma.stock_transactionsInclude = {}> =
+  Prisma.stock_transactionsGetPayload<{ include: T }>
+export type WalletTransaction<T extends Prisma.wallet_transactionsInclude = {}> =
+  Prisma.wallet_transactionsGetPayload<{ include: T }>
