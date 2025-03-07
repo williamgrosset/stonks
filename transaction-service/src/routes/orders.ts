ccb07fe16b82af605b3ab66136cce60b57f3ba10
William Grosset <williamhgrosset@gmail.com>
2025-03-06 18:10:40 -0800

    Revert "Parallelize queries in transaction"
    This reverts commit 705f117d6a926905bcb5e9d9a024f0cd2d8cdfae.

--- transaction-service/src/routes/orders.ts
+++ transaction-service/src/routes/orders.ts
@@ -84,10 +84,10 @@ async function routes(fastify: FastifyInstance) {
       return reply.status(500).send({ success: false, data: { error: 'Internal server error' } })
     }
   })
+
   fastify.post<{ Body: CompleteOrderBody }>('/orders/complete', async (request, reply) => {
     const { buyer_id, seller_id, stock_transaction_id, stock_id, price, quantity, is_partial } =
       request.body
-
     const buyerIdInt = parseInt(buyer_id)
     const sellerIdInt = parseInt(seller_id)
     const stockTransactionIdInt = parseInt(stock_transaction_id)
@@ -96,82 +96,103 @@ async function routes(fastify: FastifyInstance) {
     try {
       await prisma.$transaction(
         async tx => {
-          // Add shares to buyer and update seller wallet balance
-          await Promise.all([
-            tx.shares.upsert({
-              where: { stock_id_user_id: { stock_id: stockIdInt, user_id: buyerIdInt } },
-              update: { quantity: { increment: quantity } },
-              create: { stock_id: stockIdInt, user_id: buyerIdInt, quantity }
-            }),
-
-            tx.users.update({
-              where: { id: sellerIdInt },
-              data: { wallet_balance: { increment: price * quantity } }
-            })
-          ])
-
-          // Create wallet transactions
-          const [buyerWalletTx, sellerWalletTx] = await Promise.all([
-            tx.wallet_transactions.create({
-              data: { user_id: buyerIdInt, is_debit: true, amount: price * quantity }
-            }),
-
-            tx.wallet_transactions.create({
-              data: { user_id: sellerIdInt, is_debit: false, amount: price * quantity }
-            })
-          ])
-
-          // Create stock transactions
-          await Promise.all([
-            tx.stock_transactions.create({
-              data: {
+          // ---------- BUYER -----------
+          // Add shares to user
+          await tx.shares.upsert({
+            where: {
+              stock_id_user_id: {
                 stock_id: stockIdInt,
-                user_id: buyerIdInt,
-                wallet_transaction_id: buyerWalletTx.id,
+                user_id: buyerIdInt
+              }
+            },
+            update: {
+              quantity: { increment: quantity }
+            },
+            create: {
+              stock_id: stockIdInt,
+              user_id: buyerIdInt,
+              quantity
+            }
+          })
+
+          // Create wallet transaction
+          const buyerWalletTx = await tx.wallet_transactions.create({
+            data: {
+              user_id: buyerIdInt,
+              is_debit: true,
+              amount: price * quantity
+            }
+          })
+
+          // Create `COMPLETED` stock transaction
+          await tx.stock_transactions.create({
+            data: {
+              stock_id: stockIdInt,
+              user_id: buyerIdInt,
+              wallet_transaction_id: buyerWalletTx.id,
+              order_status: 'COMPLETED',
+              order_type: 'MARKET',
+              quantity,
+              price
+            }
+          })
+
+          // ---------- SELLER -----------
+          // Add funds to user
+          await tx.users.update({
+            where: { id: sellerIdInt },
+            data: {
+              wallet_balance: { increment: price * quantity }
+            }
+          })
+
+          // Create wallet transaction
+          const sellerWalletTx = await tx.wallet_transactions.create({
+            data: {
+              user_id: sellerIdInt,
+              is_debit: false,
+              amount: price * quantity
+            }
+          })
+
+          if (is_partial) {
+            // Create `COMPLETED` child stock transaction
+            await tx.stock_transactions.create({
+              data: {
+                parent_stock_transaction_id: stockTransactionIdInt,
+                stock_id: stockIdInt,
+                user_id: sellerIdInt,
+                wallet_transaction_id: sellerWalletTx.id,
                 order_status: 'COMPLETED',
-                order_type: 'MARKET',
+                order_type: 'LIMIT',
                 quantity,
                 price
               }
-            }),
+            })
 
-            // Create `COMPLETED` child stock transaction if partial
-            is_partial
-              ? tx.stock_transactions.create({
-                  data: {
-                    parent_stock_transaction_id: stockTransactionIdInt,
-                    stock_id: stockIdInt,
-                    user_id: sellerIdInt,
-                    wallet_transaction_id: sellerWalletTx.id,
-                    order_status: 'COMPLETED',
-                    order_type: 'LIMIT',
-                    quantity,
-                    price
-                  }
-                })
-              : null
-          ])
-
-          // Update stock transaction status
-          if (is_partial) {
-            return tx.stock_transactions.update({
+            // Update stock transaction to `PARTIALLY_COMPLETE`
+            return await tx.stock_transactions.update({
               where: { id: stockTransactionIdInt },
               data: { order_status: 'PARTIALLY_COMPLETE' }
             })
           } else {
-            return tx.stock_transactions.update({
+            // Update stock transaction to `COMPLETED`
+            return await tx.stock_transactions.update({
               where: { id: stockTransactionIdInt },
               data: { order_status: 'COMPLETED', wallet_transaction_id: sellerWalletTx.id }
             })
           }
         },
-        { maxWait: 40000, timeout: 50000 }
+        {
+          maxWait: 40000,
+          timeout: 50000
+        }
       )
 
-      return reply.send({ success: true, message: 'Order completed successfully' })
+      return reply.send({ success: true, data: null })
     } catch (error) {
       console.error('Error completing order:', error)
-      return reply.status(500).send({ success: false, error: 'Internal server error' })
+      return reply.status(500).send({ success: false, data: { error: 'Internal server error' } })
     }
   })
 }
